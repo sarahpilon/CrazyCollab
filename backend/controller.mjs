5ec90735aac74c0b9d6f07ea44177aad3c63cead
dotdyl <dotdyldev@gmail.com>
2026-02-19 17:23:48 -0800

    Added more route handler setups

--- backend/controller.mjs
+++ backend/controller.mjs
@@ -12,13 +12,27 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
 
-
+// Logging into database / account
 app.post('/collab/login', asyncHandler( async (req, res) => {
 
     // Call database component to verify and get account credentials / data
     res.status(200).send("Logged in");
 }));
 
+// Saving your schedule to database
 app.post('/collab/schedule', asyncHandler( async (req, res) => {
     
+
+}));
+
+// Retrieving your schedule to database
+app.get('/collab/schedule', asyncHandler( async (req, res) => {
+
+
+}));
+
+// Connecting to a room
+app.get('/collab/#sessionid', asyncHandler( async (req, res) => {
+
+
 }));
